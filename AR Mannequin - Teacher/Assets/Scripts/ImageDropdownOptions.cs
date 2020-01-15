e5d87e430594e2b16427056911be2c09836b819e
Silver Xu <zxm37@mail.missouri.edu>
2020-01-15 15:42:41 -0800

    fix issue with network functionality of image display

--- AR Mannequin - Teacher/Assets/Scripts/ImageDropdownOptions.cs
+++ AR Mannequin - Teacher/Assets/Scripts/ImageDropdownOptions.cs
@@ -12,13 +12,31 @@ public class ImageDropdownOptions : MonoBehaviour
     public static ScanRepo.ScanType type = ScanRepo.ScanType.CT;
     [SerializeField]
     private GameObject dropdown;
+    private byte evCode;
 
     public void ChangeDropDownOption(String buttonType)
     {
         //Set image type by click on buttons
         type = (ScanRepo.ScanType)Enum.Parse(typeof(ScanRepo.ScanType), buttonType);
+        
+        UIElement ImageDropdownUI = dropdown.GetComponent<UIElement>();
         ImageDropdown = dropdown.GetComponent<Dropdown>();
         ImageDropdown.ClearOptions();
+        
+        //change evCode based on image type seleceted
+        switch (buttonType)
+        {
+            case "CT":
+                evCode = 25;
+                break;
+            case "XRAY":
+                evCode = 24;
+                break;
+            case "ULTRASOUND":
+                evCode = 31;
+                break;
+        }
+        ImageDropdownUI.ChangeEventCode(evCode);
         
         List<Dropdown.OptionData> imageOptions = new List<Dropdown.OptionData>();
         Dictionary<string, List<string>> images = ScanRepo.GetImageLookup();
