9dc863737c7f2ae702ad4845222504e04bc0b75d
Degly Pava <dspavap@unal.edu.co>
2020-09-10 13:40:26 -0400

    Nickname Available on Facilitator

---
+++ AR_Mannequin/Assets/Scripts/Photon Scripts/PlayerName.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+public class PlayerName : MonoBehaviour
+{
+    public InputField firstname;
+    public InputField lastname;
+    public Button setNameBtn;
+
+    public void OnTextFieldChange()
+    {
+        if (firstname.text.Length > 2 && lastname.text.Length > 2)
+        {
+            setNameBtn.interactable = true;
+        }
+    }
+
+    public void OnClick_SetName()
+    {
+        PhotonNetwork.NickName = firstname.text; 
+    }
+}
