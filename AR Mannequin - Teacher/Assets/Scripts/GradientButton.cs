db534a0494281058f63625217d9ee030f06e6da3
Degly Pava <dspavap@unal.edu.co>
2020-09-28 20:37:35 -0400

    Ui Update done on room connect

---
+++ AR Mannequin - Teacher/Assets/Scripts/GradientButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientButton : MonoBehaviour
+{
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+    Color GradientColor1 = new Color(220, 246, 255);
+    Color GradientColor2 = new Color(224, 245, 252);
+
+    void Start()
+    {
+        gradient = new Gradient();
+
+        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
+        colorKey = new GradientColorKey[2];
+        colorKey[0].color = GradientColor1;
+        colorKey[0].time = 0.0f;
+        colorKey[1].color = GradientColor2;
+        colorKey[1].time = 1.0f;
+
+        // Populate the alpha  keys at relative time 0 and 1  (0 and 100%)
+        alphaKey = new GradientAlphaKey[2];
+        alphaKey[0].alpha = 1.0f;
+        alphaKey[0].time = 0.0f;
+        alphaKey[1].alpha = 0.0f;
+        alphaKey[1].time = 1.0f;
+
+        gradient.SetKeys(colorKey, alphaKey);
+
+    }
+}
