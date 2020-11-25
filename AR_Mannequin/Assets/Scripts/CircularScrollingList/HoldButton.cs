c4ce4ee785f6b90ed10715e7baaf9201a27ed520
Degly Pava <dspavap@unal.edu.co>
2020-11-25 14:58:10 -0500

    New bug fixes

---
+++ AR_Mannequin/Assets/Scripts/CircularScrollingList/HoldButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public bool buttonPressed;
+    public AudioSource metronome;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+        metronome.mute = false;
+        Debug.Log("Doing chest compressions");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+        metronome.mute = true;
+        Debug.Log("Chest released");
+    }
+}
